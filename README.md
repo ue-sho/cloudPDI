fec92c6d002deaabb91aa5f62b0fed3beb1b45e6
ue-sho <tssu45@gmail.com>
2024-11-11 16:49:34 +0900

    Add Orthanc server

--- README.md
+++ README.md
@@ -6,9 +6,10 @@ https://www.ihe-j.org/file2/docs/IHE-J-cloudPDI-ver2.0n.pdf
 
 ## 簡易構成の概要
 
-- FHIRサーバー（Repositoryの役割）：FHIR APIで医療データを保存する中央リポジトリ
-    - [HAPI FHIR](https://hapifhir.io/)を使用する
-- Sender：医療データを作成してFHIRサーバーに送信するクライアント
-- Recipient：FHIRサーバーからデータを取得するクライアント
-- Authorization Server：今回はシンプルなセットアップのために省略する
-
+- FHIR Server（リポジトリの役割）：患者情報や画像メタデータなど医療データの管理を行う中央リポジトリとして機能
+    - 使用ツール：[HAPI FHIR](https://hapifhir.io/)（オープンソースのFHIRサーバー）
+- DICOMweb Server（画像リポジトリの役割）：DICOM画像データそのものを保存し、管理する中央リポジトリとして機能
+    - 使用ツール：[Orthanc](https://www.orthanc-server.com/)（オープンソースのDICOMサーバー）
+- Sender：医療データを作成し、FHIRサーバーおよびDICOMwebサーバーに送信するクライアント
+- Recipient：FHIRサーバーおよびDICOMwebサーバーから必要なデータを取得するクライアント
+- Authorization Server：今回はシンプルなセットアップのため省略しますが、OAuth2などの仕組みでアクセス制御を実装することが可能
--- sender/main.py
+++ sender/main.py
@@ -2,20 +2,54 @@ import sys
 import requests
 
 fhir_server_url = "http://localhost:8080/fhir"
+dicom_server_url = "http://localhost:8042/instances"
 
 
-def read_dicom_file(file_path: str):
+def read_dicom_file(file_path: str) -> bytes:
     with open(file_path, "rb") as f:
         return f.read()
 
 
-def request_fhir_server(path: str, method: str, data=None):
+def send_to_dicom_server(data: bytes) -> str | None:
     headers = {"Content-Type": "application/dicom"}
-    response = requests.request(method, fhir_server_url + path, headers=headers, data=data)
-    if response.status_code == 201:
-        print("DICOMデータがFHIRサーバーに保存されました。")
+    response = requests.post(dicom_server_url, headers=headers, data=data)
+    if response.status_code == 200:
+        print("DICOMデータがOrthancサーバーに保存されました。")
+        dicom_id = response.json()["ID"]
+        print("DICOM ID:", dicom_id)
+        return dicom_id
     else:
-        print("エラーが発生しました:", response.status_code)
+        print("エラーが発生しました(Orthancサーバー):", response.status_code)
+        print(response.text)
+        return None
+
+
+def send_to_fhir_server(dicom_id: str):
+    imaging_study_data = {
+        "resourceType": "ImagingStudy",
+        "status": "available",
+        "description": "Uploaded DICOM study",
+        "series": [
+            {
+                "uid": dicom_id,
+                "instance": [
+                    {
+                        "uid": dicom_id,
+                        "url": f"{dicom_server_url}/{dicom_id}",
+                    }
+                ],
+            }
+        ],
+    }
+
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(
+        f"{fhir_server_url}/ImagingStudy", headers=headers, json=imaging_study_data
+    )
+    if response.status_code == 201:
+        print("ImagingStudyがFHIRサーバーに保存されました。")
+    else:
+        print("エラーが発生しました(FHIRサーバー):", response.status_code)
         print(response.text)
 
 
@@ -26,4 +60,8 @@ if __name__ == "__main__":
 
     dicom_file_path = sys.argv[1]
     dicom_data = read_dicom_file(dicom_file_path)
-    request_fhir_server("/Binary", "POST", data=dicom_data)
+
+    dicom_id = send_to_dicom_server(dicom_data)
+
+    if dicom_id:
+        send_to_fhir_server(dicom_id)
